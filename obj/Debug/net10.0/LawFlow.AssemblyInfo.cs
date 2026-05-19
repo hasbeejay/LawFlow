4ba07808beffe900fe56127a79c16d893b965361
AbdulRaheem <raheemxghumman@gmail.com>
2026-05-19 23:43:40 +0500

    Data base reseeded with sample test data

--- obj/Debug/net10.0/LawFlow.AssemblyInfo.cs
+++ obj/Debug/net10.0/LawFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LawFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079e28e00fe2df28aa61b8a0bfc4fe641176bcbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a811b9abada89251fc3274b8084aaf02d7d39ce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LawFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LawFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
