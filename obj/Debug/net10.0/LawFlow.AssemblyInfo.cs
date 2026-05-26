0b2be7f3f0a1831c8c5178407d38d333adc018f1
Haseeb Jalil <hasbeejayleo@gmail.com>
2026-05-26 20:23:30 +0500

    Bump assembly info and rebuild artifacts
    Update AssemblyInformationalVersion and SourceLink to new commit hash (1.0.0+2f917808...) and regenerate build outputs. Binary and PDB files under bin/Debug/net10.0 and corresponding obj/* files/caches were updated as part of a rebuild; no source code logic changes.

--- obj/Debug/net10.0/LawFlow.AssemblyInfo.cs
+++ obj/Debug/net10.0/LawFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LawFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23b855e567ee10215b56a7f021d672dd3625553")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f917808cba501a0b9a3128c9bf4402ea05ba3dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LawFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LawFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
